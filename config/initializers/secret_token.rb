f5d75d6ae803bdea41d6114d8d068ac0f713ba75
sebsoler <sebastian.a.soler@gmail.com>
2011-04-13 23:05:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupiverse::Application.config.secret_token = 'fbb21b457867a77239618c0dfaaaced8dfd30b470978856b4c3a3931f2d95c5f5c2417d0c8d7ae0ecfa11be03eb3d02376d879deef80095722fc4948187e2c09'
