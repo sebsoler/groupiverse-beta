14f5d401a72114376a7fdb2360879ec48aa37d95
sebsoler <sebastian.a.soler@gmail.com>
2011-04-20 00:47:22 -0400

    Added a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Return title
+  def title
+    base_title = "Groupiverse Beta App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
